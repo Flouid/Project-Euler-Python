8cb408c48e2ec2e95a5133d8794b75ed79c9f9a9
Flouid <louiskeith99@gmail.com>
2022-02-11 21:01:29 -0500

    added template for creating new scripts

---
+++ template.py
@@ -0,0 +1,12 @@
+"""
+Louis Keith
+DATE HERE
+"""
+
+
+from time import time
+    
+
+if __name__ == '__main__':
+    start = time()
+    print('\nFINISHED IN %s SECONDS' % round(time() - start, 4))
