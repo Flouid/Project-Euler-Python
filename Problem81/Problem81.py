889350df5446a79a7d8a83d0b87bab634abd484a
Flouid <louiskeith99@gmail.com>
2022-02-11 19:18:55 -0500

    added test file and nonfunctional implementation

--- Problem81/Problem81.py
+++ Problem81/Problem81.py
@@ -16,10 +16,78 @@ Louis Keith
 """
 
 from time import time
+from turtle import back
+
+
+def ingest_data(filename='matrix.txt'):
+    """Ingest the matrix from disk and split it into a nested list.
+    Each element is a list representing a row in the matrix."""
+    with open(filename) as f:
+        # read rows
+        data = f.read().split('\n')
+        # split rows into individual lists of integers
+        data = [list(map(int, line.split(','))) for line in data if line != '']
+    return data
+
+
+def get_path_sum(matrix):
+    r, c = 0, 0
+    r_end, c_end = len(matrix)-1, len(matrix[0])-1
+    path_sum = matrix[r][c]
+
+    while (r, c) != (r_end, c_end):
+        # check if on bottom row, if so then move right
+        if r == r_end:
+            c += 1
+        # check if on right wall, if so then move down
+        elif c == c_end:
+            r += 1
+        # check if number beneath is less than number to right, if so then move down
+        elif matrix[r+1][c] < matrix[r][c+1]:
+            r += 1
+        # otherwise then move right
+        else:
+            c += 1
+        path_sum += matrix[r][c]
+        print('FORWARD: ', matrix[r][c], path_sum)
+
+    return path_sum
+
+
+
+def get_backward_sum(matrix):
+    r, c = len(matrix)-1, len(matrix[0])-1
+    r_end, c_end = 0, 0
+    path_sum = matrix[r][c]
+
+    while (r, c) != (r_end, c_end):
+        # check if on top row, if so then move left
+        if r == r_end:
+            c -= 1
+        # check if on left wall, if so then move up
+        elif c == c_end:
+            r -= 1
+        # check if number above is less than number to left, if so then move up
+        elif matrix[r-1][c] < matrix[r][c-1]:
+            r -= 1
+        # otherwise then move left
+        else:
+            c -= 1
+        path_sum += matrix[r][c]
+        print('BACKWARD: ', matrix[r][c], path_sum)
+
+    return path_sum
+
 
 
 def main():
-    return
+    matrix = ingest_data('test_matrix.txt')
+    forward_sum = get_path_sum(matrix)
+    print()
+    backward_sum = get_backward_sum(matrix)
+
+    print(min(forward_sum, backward_sum))
+    
 
 
 if __name__ == '__main__':
