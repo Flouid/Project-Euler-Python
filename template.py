4668484687dcf8718958d95cce8c026732544943
Flouid <louiskeith99@gmail.com>
2022-02-14 14:22:49 -0500

    template update with verbose mode

--- template.py
+++ template.py
@@ -5,8 +5,18 @@ DATE HERE
 
 
 from time import time
+import sys
     
 
+def main():
+    print('hello world')
+
+
 if __name__ == '__main__':
+    verbose = False
+    if len(sys.argv) == 2 and sys.argv[1] == '-v':
+        verbose = True
+
     start = time()
+    main()
     print('\nFINISHED IN %s SECONDS' % round(time() - start, 4))
