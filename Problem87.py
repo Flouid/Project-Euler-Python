0f896934b608b0edf5874c803d36ca47123101eb
Flouid <louiskeith99@gmail.com>
2022-02-17 19:34:29 -0500

    have it down to 3.5 seconds using sets

--- Problem87.py
+++ Problem87.py
@@ -34,27 +34,26 @@ def find_primes(threshold):
 
 
 def find_num_triples(threshold, primes):
-    """Use a dynamic programming approach to populate the entire solution space.
-    Returns the number of unique solutions found in that solution space."""
+    """Find the number of numbers that can be expressed as the sum of a square, cube, and fourth power
+    of primes below a threshold. Takes a list of primes to use and the threshold as parameters."""
     # save computation time by calculating the primes to each power of interest ahead of time
     power2 = [pow(prime, 2) for prime in primes]
     power3 = [pow(prime, 3) for prime in primes]
     power4 = [pow(prime, 4) for prime in primes]
     len_primes = len(primes)
-    # create a 3D matrix where the coordinates of each element represent the combination
-    # of primes required to generate that element, initialize it with zeros at first.
-    solution_space = np.zeros((len_primes, len_primes, len_primes), dtype=int)
+    # store the sums found in a set as to eliminate duplicate sums
+    sums = set()
 
-    # populate the matrix
+    # search the entire solution space
     for k in range(len_primes):
         for j in range(len_primes):
             for i in range(len_primes):
                 sum = power2[i] + power3[j] + power4[k]
                 if sum < threshold:
+                    sums.add(sum)
                     # debugging statement
                     if verbose:
                         print('%d = %d^2 + %d^3 + %d^4' % (sum, primes[i], primes[j], primes[k]))
-                    solution_space[i, j, k] = sum
                 # if at any point the sum exceeds the threshold, stop populating along that axis
                 else:
                     break
@@ -62,11 +61,7 @@ def find_num_triples(threshold, primes):
                 break
         if power2[0] + power3[0] + power4[k] >= threshold:
             break
-    # calculating the number of unique elements takes some time
-    if verbose:
-        print('Counting unique elements of the solution space...')
-    # don't consider 0 as a solution, so subtract 1
-    return len(np.unique(solution_space)) - 1
+    return len(sums)
 
 
 def main():
